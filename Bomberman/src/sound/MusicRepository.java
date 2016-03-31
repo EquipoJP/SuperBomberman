b074df4a344c3a4e2a9a570ccd2eddf365ac4d25
Patricia Lazaro <plazaro1994@gmail.com>
2016-03-31 17:52:14 +0200

    music loaded

--- Bomberman/src/sound/MusicRepository.java
+++ Bomberman/src/sound/MusicRepository.java
@@ -3,19 +3,78 @@ package sound;
 import java.io.File;
 
 import kuusisto.tinysound.Music;
+import kuusisto.tinysound.Sound;
 import kuusisto.tinysound.TinySound;
 
 public class MusicRepository {
 	
 	public static Music battle = null;
+	public static Music credits = null;
+	public static Music gameOver = null;
+	public static Music menu = null;
+	public static Music pause = null;
+	
+	public static Sound defeat = null;
+	public static Sound intro = null;
+	public static Sound victory = null;
 	
 	public static void load(){
 		loadBattleMusic();
+		loadCreditsMusic();
+		loadGameOverMusic();
+		loadMenuMusic();
+		loadPauseMusic();
+		
+		loadDefeatSound();
+		loadIntroSound();
+		loadVictorySound();
 	}
 
 	private static void loadBattleMusic() {
 		if(battle == null){
 			battle = TinySound.loadMusic(new File(SoundTrack.BATTLE_MUSIC));
+		}
+	}
+	
+	private static void loadCreditsMusic() {
+		if(credits == null){
+			credits = TinySound.loadMusic(new File(SoundTrack.CREDITS_MUSIC));
+		}
+	}
+	
+	private static void loadGameOverMusic() {
+		if(gameOver == null){
+			gameOver = TinySound.loadMusic(new File(SoundTrack.GAMEOVER_MUSIC));
+		}
+	}
+	
+	private static void loadMenuMusic() {
+		if(menu == null){
+			menu = TinySound.loadMusic(new File(SoundTrack.MENU_MUSIC));
+		}
+	}
+	
+	private static void loadPauseMusic() {
+		if(pause == null){
+			pause = TinySound.loadMusic(new File(SoundTrack.PAUSE_MUSIC));
+		}
+	}
+	
+	private static void loadDefeatSound() {
+		if(defeat == null){
+			defeat = TinySound.loadSound(new File(SoundTrack.DEFEAT_SND));
+		}
+	}
+	
+	private static void loadIntroSound() {
+		if(intro == null){
+			intro = TinySound.loadSound(new File(SoundTrack.INTRO_SND));
+		}
+	}
+	
+	private static void loadVictorySound() {
+		if(victory == null){
+			victory = TinySound.loadSound(new File(SoundTrack.VICTORY_SND));
 		}
 	}
 
--- Bomberman/src/sound/SoundTrack.java
+++ Bomberman/src/sound/SoundTrack.java
@@ -3,6 +3,17 @@ package sound;
 public class SoundTrack {
 	
 	private static final String AUDIO = "resources/audio/";
-	public static final String BATTLE_MUSIC = AUDIO + "battle.wav";
+	private static final String MUSIC = AUDIO + "music/";
+	private static final String SOUND = AUDIO + "sound/";
+	
+	public static final String BATTLE_MUSIC = MUSIC + "battle.wav";
+	public static final String CREDITS_MUSIC = MUSIC + "credits.ogg";
+	public static final String GAMEOVER_MUSIC = MUSIC + "gameOver.ogg";
+	public static final String MENU_MUSIC = MUSIC + "menus.ogg";
+	public static final String PAUSE_MUSIC = MUSIC + "pause.ogg";
+	
+	public static final String DEFEAT_SND = SOUND + "defeat.wav";
+	public static final String INTRO_SND = SOUND + "introStage.wav";
+	public static final String VICTORY_SND = SOUND + "victoryStage.wav";
 
 }
