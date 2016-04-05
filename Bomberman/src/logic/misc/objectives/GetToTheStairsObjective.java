3ba8c02bf94639228bce7b849424b962829c45f2
Patricia Lazaro <plazaro1994@gmail.com>
2016-04-05 12:50:11 +0200

    objectives (falta)

--- Bomberman/src/logic/misc/objectives/GetToTheStairsObjective.java
+++ Bomberman/src/logic/misc/objectives/GetToTheStairsObjective.java
@@ -1,13 +1,22 @@
 package logic.misc.objectives;
 
 import graphics.rooms.Room;
+import logic.Objeto;
+import logic.characters.Enemy;
 
 public class GetToTheStairsObjective extends Objective{
 
 	@Override
 	public boolean test(Room room) {
+		for (Objeto obj : room.objetos) {
+			if (obj instanceof Enemy) {
+				return false;
+			}
+		}
+		
+		// Check if player got to the stairs
 		// TODO
-		return false;
+		return true;
 	}
 	
 }
