8044209470ab28e1aa009a5945471204e7f6eee5
Jaime <jai_mi_to@hotmail.es>
2016-04-07 11:52:00 +0200

    Started to introduce another Bomberman

---
+++ Bomberman/src/logic/characters/CPUBomber.java
@@ -0,0 +1,14 @@
+package logic.characters;
+
+import graphics.rooms.Room;
+import main.Initialization.STAGE;
+
+public class CPUBomber extends Enemy {
+
+	public CPUBomber(int x, int y, int z, Room r, STAGE stage) {
+		super(x, y, z, r, stage);
+	}
+
+	
+	
+}
