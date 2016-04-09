5ab7a27f53ff3e76cbad28797ee5cb341006cf72
Patricia Lazaro <plazaro1994@gmail.com>
2016-04-09 17:20:53 +0200

    graphics.rooms.optionsMenu comentado

--- Bomberman/src/graphics/rooms/optionsMenu/OptionsMenu.java
+++ Bomberman/src/graphics/rooms/optionsMenu/OptionsMenu.java
@@ -1,5 +1,5 @@
 /**
- * Class representing the options menu
+ * Class representing the sound options menu
  */
 package graphics.rooms.optionsMenu;
 
@@ -23,58 +23,69 @@ import logic.StatesMachine.STATE;
  * @author Jaime Ruiz-Borau Vizarraga (546751)
  */
 public class OptionsMenu extends Room {
-	
+
 	private static final int PADDING_BORDER = 25;
 
+	/**
+	 * @param w
+	 *            width
+	 * @param h
+	 *            height
+	 * @param n
+	 *            name
+	 */
 	public OptionsMenu(int w, int h, String n) {
 		super(w, h, n);
-		// TODO Auto-generated constructor stub
 	}
-	
+
 	@Override
 	public void load() {
 		OptionsMenuRepository.load();
-		
+
 		background = OptionsMenuRepository.background;
-		
+
 		int x = width / 2;
 
 		Sprite back = OptionsMenuRepository.backButton;
-		back.setSubsprites(new BufferedImage[] { back.getSubsprites()[back.getSubimages()-1] });
+		back.setSubsprites(new BufferedImage[] { back.getSubsprites()[back
+				.getSubimages() - 1] });
 		back.setSubimages(1);
 		int y = this.height - PADDING_BORDER - back.getHeight() / 2;
 		addObjeto(new Visual(x, y, this, back));
-		
-		y = height/2;
+
+		y = height / 2;
 		Sprite slider = OptionsMenuRepository.slider;
-		
+
 		int img_index = getImageIndex();
-		
+
 		addObjeto(new Slider(x, y, this, slider, new VolumeSlider(), img_index));
 
 		Sprite title = OptionsMenuRepository.titleButton;
 		y = PADDING_BORDER + title.getHeight() / 2;
 		addObjeto(new Visual(x, y, this, title));
-		
+
 		setMusic(MusicRepository.menu, true);
 	}
-	
-	private int getImageIndex(){
+
+	/**
+	 * @return gets the image index of the slider from the global volume
+	 */
+	private int getImageIndex() {
 		double volume = TinySound.getGlobalVolume();
 		int img_index = 0;
-		
-		if(volume >= 1.0 && volume < 1.25){
+
+		if (volume >= 1.0 && volume < 1.25) {
 			img_index = 0;
-		} else if(volume >= 1.25 && volume < 1.5){
+		} else if (volume >= 1.25 && volume < 1.5) {
 			img_index = 1;
-		} else if(volume >= 1.5 && volume < 1.75){
+		} else if (volume >= 1.5 && volume < 1.75) {
 			img_index = 2;
-		} else if(volume >= 1.75 && volume < 2.0){
+		} else if (volume >= 1.75 && volume < 2.0) {
 			img_index = 3;
-		} else if(volume >= 2.0){
+		} else if (volume >= 2.0) {
 			img_index = 4;
 		}
-		
+
 		return img_index;
 	}
 
@@ -88,7 +99,10 @@ public class OptionsMenu extends Room {
 			g.drawImage(background.getSubsprites()[0], 0, 0, null);
 		}
 	}
-	
+
+	/**
+	 * Confirms
+	 */
 	private void confirm() {
 		StatesMachine.goToRoom(STATE.MAIN_MENU, false);
 	}
--- Bomberman/src/graphics/rooms/optionsMenu/OptionsMenuRepository.java
+++ Bomberman/src/graphics/rooms/optionsMenu/OptionsMenuRepository.java
@@ -1,16 +1,26 @@
+/**
+ * Class containing the resources for the sound options menu
+ */
 package graphics.rooms.optionsMenu;
 
 import logic.Sprite;
 import main.Initialization;
 
+/**
+ * @author Patricia Lazaro Tello (554309)
+ * @author Jaime Ruiz-Borau Vizarraga (546751)
+ */
 public class OptionsMenuRepository {
-	
+
 	static Sprite background = null;
 
 	static Sprite titleButton = null;
 	static Sprite slider = null;
 	static Sprite backButton = null;
 
+	/**
+	 * Load resources
+	 */
 	public static void load() {
 		loadBackground();
 		loadTitleButton();
@@ -18,27 +28,46 @@ public class OptionsMenuRepository {
 		loadBackButton();
 	}
 
+	/**
+	 * Load background
+	 */
 	private static void loadBackground() {
-		if(background == null){
-			background = Initialization.getSpriteFromMenu(Initialization.MENUS.BACKGROUND.toString());
+		if (background == null) {
+			background = Initialization
+					.getSpriteFromMenu(Initialization.MENUS.BACKGROUND
+							.toString());
 		}
 	}
 
+	/**
+	 * Load title button
+	 */
 	private static void loadTitleButton() {
 		if (titleButton == null) {
-			titleButton = Initialization.getSpriteFromMenu(Initialization.MENUS.TITLE_BUTTON.toString());
+			titleButton = Initialization
+					.getSpriteFromMenu(Initialization.MENUS.TITLE_BUTTON
+							.toString());
 		}
 	}
-	
-	private static void loadSlider(){
-		if(slider == null){
-			slider = Initialization.getSpriteFromMenu(Initialization.MENUS.SLIDER.toString());
+
+	/**
+	 * Load slider
+	 */
+	private static void loadSlider() {
+		if (slider == null) {
+			slider = Initialization
+					.getSpriteFromMenu(Initialization.MENUS.SLIDER.toString());
 		}
 	}
-	
+
+	/**
+	 * Load back button
+	 */
 	private static void loadBackButton() {
 		if (backButton == null) {
-			backButton = Initialization.getSpriteFromMenu(Initialization.MENUS.BACK_BUTTON.toString());
+			backButton = Initialization
+					.getSpriteFromMenu(Initialization.MENUS.BACK_BUTTON
+							.toString());
 		}
 	}
 
