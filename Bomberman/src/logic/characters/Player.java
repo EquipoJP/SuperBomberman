8a0430f19028aa466fe4071eead1c1c227c7dbc0
Jaime <jai_mi_to@hotmail.es>
2016-04-16 13:50:54 +0200

    Bruh nada, no ha habido forma

--- Bomberman/src/logic/characters/Player.java
+++ Bomberman/src/logic/characters/Player.java
@@ -27,11 +27,7 @@ import sound.MusicRepository;
  */
 public class Player extends Objeto {
 
-<<<<<<< HEAD
 	/* Info Sprites */
-=======
-	/* Info for getting Sprites LOL MENUDO MERGE */
->>>>>>> master
 	private Map<String, Sprite> sprites;
 
 	private static final int HELP_THRESHOLD = 8;
