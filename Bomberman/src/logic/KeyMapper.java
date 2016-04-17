4022797ea84a0b5fc402078fe3ae8c116437c6f4
Jaime <jai_mi_to@hotmail.es>
2016-04-17 13:03:16 +0200

    Pf que la he liado un poco y ya lo he dejado bien

--- Bomberman/src/logic/KeyMapper.java
+++ Bomberman/src/logic/KeyMapper.java
@@ -11,38 +11,38 @@ import utils.IniUtils;
 import utils.SaveSystemService;
 
 public class KeyMapper {
-	
+
 	private static final String KEYMAPPER_FILE = "keymap.ini";
 	private static final String KM_SECTION = "default";
-	
+
 	private Map<KEY, Integer> mapper;
-	
-	public KeyMapper(){
+
+	public KeyMapper() {
 		mapper = new HashMap<KEY, Integer>();
-		
-		if(!loadKeyMapper()){
+
+		if (!loadKeyMapper()) {
 			// Create initial association
 			createKeyMapper();
 		}
 	}
-	
-	public int getCodeKey(KEY key){
+
+	public int getCodeKey(KEY key) {
 		return mapper.get(key);
 	}
-	
-	public void changeKeyCode(KEY key, int code){
+
+	public void changeKeyCode(KEY key, int code) {
 		mapper.put(key, code);
 		saveKeyMapper();
 	}
-	
-	private boolean loadKeyMapper(){
+
+	private boolean loadKeyMapper() {
 		String file = SaveSystemService.PATH + KEYMAPPER_FILE;
 		File f = new File(file);
-		
-		if(f.exists() && f.isFile()){
-			for(KEY key : KEY.values()){
+
+		if (f.exists() && f.isFile()) {
+			for (KEY key : KEY.values()) {
 				String strValue = IniUtils.getValue(file, KM_SECTION, key.toString());
-				if(strValue != null){
+				if (strValue != null) {
 					mapper.put(key, Integer.parseInt(strValue));
 				}
 			}
@@ -50,49 +50,49 @@ public class KeyMapper {
 		}
 		return false;
 	}
-	
-	private boolean createKeyMapper(){
+
+	private boolean createKeyMapper() {
 		// up
 		KEY key = KEY.UP;
 		int value = KeyEvent.VK_UP;
 		mapper.put(key, value);
-		
+
 		// down
 		key = KEY.DOWN;
 		value = KeyEvent.VK_DOWN;
 		mapper.put(key, value);
-		
+
 		// left
 		key = KEY.LEFT;
 		value = KeyEvent.VK_LEFT;
 		mapper.put(key, value);
-		
+
 		// right
 		key = KEY.RIGHT;
 		value = KeyEvent.VK_RIGHT;
 		mapper.put(key, value);
-		
+
 		// bomb
 		key = KEY.BOMB;
 		value = KeyEvent.VK_SPACE;
 		mapper.put(key, value);
-		
+
 		// pause
 		key = KEY.PAUSE;
 		value = KeyEvent.VK_ESCAPE;
 		mapper.put(key, value);
-		
+
 		// confirm
 		key = KEY.CONFIRM;
 		value = KeyEvent.VK_ENTER;
 		mapper.put(key, value);
-		
+
 		return saveKeyMapper();
 	}
-	
-	public boolean saveKeyMapper(){
+
+	public boolean saveKeyMapper() {
 		String file = SaveSystemService.PATH + KEYMAPPER_FILE;
-		
+
 		File directory = new File(SaveSystemService.PATH);
 		if (directory.exists()) {
 			; // directory exists
@@ -101,23 +101,20 @@ public class KeyMapper {
 		} else {
 			return false;
 		}
-		
+
 		File f = new File(file);
 		try {
 			f.createNewFile();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
-					for(KEY key : mapper.keySet()){
-						Integer intValue = mapper.get(key);
-						IniUtils.addValue(file, KM_SECTION, key.toString(), intValue + "");
-					}
-					return true;
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
+		if (f.exists() && f.isFile()) {
+			for (KEY key : mapper.keySet()) {
+				Integer intValue = mapper.get(key);
+				IniUtils.addValue(file, KM_SECTION, key.toString(), intValue + "");
 			}
+			return true;
+
 		}
 		return false;
 	}
