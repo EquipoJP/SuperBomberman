0c49dc5977bf248a69ee9cb0a915778289844b4c
Patricia Lazaro <plazaro1994@gmail.com>
2016-04-22 11:04:59 +0200

    graphics.effects comentado

--- Bomberman/src/graphics/effects/slider/SliderHelper.java
+++ Bomberman/src/graphics/effects/slider/SliderHelper.java
@@ -3,7 +3,17 @@
  */
 package graphics.effects.slider;
 
+/**
+ * @author Patricia Lazaro Tello (554309)
+ * @author Jaime Ruiz-Borau Vizarraga (546751)
+ */
 public interface SliderHelper {
 
+	/**
+	 * Do something depending on the subimage of the slider
+	 * 
+	 * @param subimage
+	 *            subimage of the slider
+	 */
 	public void action(int subimage);
 }
